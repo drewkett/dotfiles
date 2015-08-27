01a69f57b52aa5c2b40ca21c8cca4161a872def0
Andrew Burkett <burkett.andrew@gmail.com>
2015-08-27 11:07:47 -0700

    A few more emacs changes

--- emacs.el
+++ emacs.el
@@ -1,25 +1,22 @@
 (require 'package)
 (push '("marmalade" . "http://marmalade-repo.org/packages/")
-	package-archives)
+      package-archives)
 (push '("melpa" . "http://melpa.milkbox.net/packages/")
-	package-archives)
+      package-archives)
 (package-initialize)
 
 (unless package-archive-contents
   (package-refresh-contents))
 
-(setq package-list '(evil magit helm cider smartparens auctex))
-
-; install the missing packages
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+(let ((packages-list '(evil magit helm cider smartparens auctex workgroups2)))
+  (dolist (package package-list)
+    (unless (package-installed-p package)
+      (package-install package))))
 
 (require 'evil)
 (evil-mode 1)
 
 (load-theme 'tango-dark)	
-;(load-theme 'monokai)
 
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x c") 'cvs-status)
@@ -38,6 +35,8 @@
 (add-to-list 'evil-emacs-state-modes 'magit-popup-sequence-mode)
 (add-to-list 'evil-emacs-state-modes 'cvs-mode)
 (add-to-list 'evil-emacs-state-modes 'term-mode)
+(add-to-list 'evil-emacs-state-modes 'image-mode)
+(add-to-list 'evil-emacs-state-modes 'image-dired-thumbnail-mode)
 
 (setq cider-auto-select-error-buffer nil)
 
@@ -69,22 +68,23 @@
 (setq reftex-plug-into-AUCTeX t)
 
 (require 'smartparens-config)
-;(smartparens-global-mode t) 
+					;(smartparens-global-mode t) 
 (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
 (add-hook 'cider-repl-mode-hook 'turn-on-smartparens-strict-mode)
 (sp-use-smartparens-bindings)
-					;
-;(custom-set-variables
-; ;; custom-set-variables was added by Custom.
-; ;; If you edit it by hand, you could mess it up, so be careful.
-; ;; Your init file should contain only one such instance.
-; ;; If there is more than one, they won't work right.
-; '(custom-safe-themes
-;   (quote
-;    ("05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default))))
-;(custom-set-faces
-; ;; custom-set-faces was added by Custom.
-; ;; If you edit it by hand, you could mess it up, so be careful.
-; ;; Your init file should contain only one such instance.
-; ;; If there is more than one, they won't work right.
-; )
+
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+			      (interactive)
+			      (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+			      (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t))
+
+(require 'ess-site)
+(ess-toggle-underscore nil)
+
