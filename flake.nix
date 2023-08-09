9ac64ab0453218e7104614effd471275de6eecaf
Andrew Burektt <burkett.andrew@gmail.com>
2023-08-08 20:22:10 -0700

    Update

--- flake.nix
+++ flake.nix
@@ -6,16 +6,19 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fantasy.url = "path:/home/andrew/code/fantasy";
   };
 
-  outputs = inputs @ { nixpkgs, home-manager, ... }: {
-      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-      homeConfigurations = {
-            "andrew" = home-manager.lib.homeManagerConfiguration {
-                pkgs = import nixpkgs { system = "x86_64-linux"; };
-
-                modules = [ ./home.nix ]; # Defined later
-            };
+  outputs = inputs @ { nixpkgs, home-manager, fantasy, ... }: {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+    homeConfigurations = {
+      "andrew" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+          modules  = [ ./home.nix ];
+          extraSpecialArgs = {
+            fantasy = fantasy;
+          };
         };
+      };
     };
 }
--- home.nix
+++ home.nix
@@ -1,19 +1,59 @@
-{pkgs, ...}: {
-    home.username = "andrew";
-    home.homeDirectory = "/home/andrew";
-    home.stateVersion = "23.05"; # To figure this out you can comment out the line and see what version it expected.
-    home.packages = [
-        pkgs.gh
-        pkgs.ripgrep
-        pkgs.bottom
-        pkgs.fd
-        pkgs.neovim
-        pkgs.borgbackup
-        #pkgs.gcc
-        #pkgs.poetry
-    ];
-    home.sessionVariables = {
-        EDITOR = "vim";
+{pkgs, fantasy, ...}: {
+  home.username = "andrew";
+  home.homeDirectory = "/home/andrew";
+  home.stateVersion = "23.05"; # To figure this out you can comment out the line and see what version it expected.
+  home.packages = [
+      pkgs.gh
+      pkgs.ripgrep
+      pkgs.bottom
+      pkgs.fd
+      pkgs.neovim
+      pkgs.borgbackup
+      #pkgs.gcc
+      #pkgs.poetry
+  ];
+  home.sessionVariables = {
+      EDITOR = "vim";
+  };
+  programs.home-manager.enable = true;
+
+  systemd.user.services.bbh = {
+    Unit.Description = "bb_hitters";
+    Install.WantedBy = [ "default.target" ];
+    Service = {
+      WorkingDirectory = "/home/andrew/code/fantasy";
+      Environment = [
+        "STREAMLIT_SERVER_HEADLESS=true"
+        "STREAMLIT_SERVER_PORT=8520"
+        "STREAMLIT_BROWSER_GATHER_USAGE_STATS=false"
+      ];
+      ExecStart = "${fantasy.apps.x86_64-linux.streamlit.program} run bin/bb_hitters_2022.py";
     };
-    programs.home-manager.enable = true;
+  };
+  systemd.user.services.bbp = {
+    Unit.Description = "bb_pitchers";
+    Install.WantedBy = [ "default.target" ];
+    Service = {
+      WorkingDirectory = "/home/andrew/code/fantasy";
+      Environment = [
+        "STREAMLIT_SERVER_HEADLESS=true"
+        "STREAMLIT_SERVER_PORT=8521"
+        "STREAMLIT_BROWSER_GATHER_USAGE_STATS=false"
+      ];
+      ExecStart = "${fantasy.apps.x86_64-linux.streamlit.program} run bin/bb_pitchers_2022.py";
+    };
+  };
+  systemd.user.services.bbid = {
+    Unit.Description = "bb_id_map";
+    Install.WantedBy = [ "default.target" ];
+    Service = {
+      WorkingDirectory = "/home/andrew/code/fantasy";
+      Environment = [
+        "STREAMLIT_SERVER_HEADLESS=true"
+        "STREAMLIT_SERVER_PORT=8522"
+        "STREAMLIT_BROWSER_GATHER_USAGE_STATS=false"
+      ];
+      ExecStart = "${fantasy.apps.x86_64-linux.streamlit.program} run bin/bb_id_map.py";
+    };
+  };
 }
