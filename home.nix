d769f586a3684b0abadd42fdb4a92d4e30865168
Andrew Burektt <burkett.andrew@gmail.com>
2023-08-15 18:28:18 -0700

    Split out programs into separate file

--- home.nix
+++ home.nix
@@ -6,6 +6,7 @@ in
 {
   imports = [
     ./systemd.nix
+    ./programs.nix
   ];
 
   home.username = "${username}";
@@ -21,36 +22,4 @@ in
       EDITOR = "vim";
   };
   programs.home-manager.enable = true;
-
-  programs.gh = {
-    enable = true;
-  };
-  programs.tmux = {
-    enable = true;
-  };
-  programs.jq = {
-    enable = true;
-  };
-  programs.bat = {
-    enable = true;
-  };
-  programs.git = {
-    enable = true;
-    userName = "Andrew Burkett";
-    userEmail = "burkett.andrew@gmail.com";
-    ignores = [ "*.swp" ];
-  };
-  programs.vim = {
-    enable = true;
-    extraConfig = (builtins.readFile ./vimrc);
-    plugins = with pkgs.vimPlugins; [
-      ctrlp
-      rust-vim
-      vim-airline
-      vim-fugitive
-      vim-colorschemes
-      vim-gitgutter
-      vim-prettier
-    ];
-  };
 }
---
+++ programs.nix
@@ -0,0 +1,34 @@
+{pkgs, ...}:
+{
+  programs.gh = {
+    enable = true;
+  };
+  programs.tmux = {
+    enable = true;
+  };
+  programs.jq = {
+    enable = true;
+  };
+  programs.bat = {
+    enable = true;
+  };
+  programs.git = {
+    enable = true;
+    userName = "Andrew Burkett";
+    userEmail = "burkett.andrew@gmail.com";
+    ignores = [ "*.swp" ];
+  };
+  programs.vim = {
+    enable = true;
+    extraConfig = (builtins.readFile ./vimrc);
+    plugins = with pkgs.vimPlugins; [
+      ctrlp
+      rust-vim
+      vim-airline
+      vim-fugitive
+      vim-colorschemes
+      vim-gitgutter
+      vim-prettier
+    ];
+  };
+}
