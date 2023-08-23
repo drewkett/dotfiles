9acdb7a1e675d26cb2578a24b348b95e42f5f5f9
Andrew Burektt <burkett.andrew@gmail.com>
2023-08-22 19:02:13 -0700

    Use flake apps for fantasy stuff

--- x86_64-linux.nix
+++ x86_64-linux.nix
@@ -1,48 +1,44 @@
 {pkgs, lib, fantasy, system, ...}: 
 let 
-  streamlit = "${fantasy.apps.${system}.streamlit.program}";
+  bb_hitters = "${fantasy.apps.${system}.bb_hitters.program}";
+  bb_pitchers = "${fantasy.apps.${system}.bb_pitchers.program}";
+  bb_id_map = "${fantasy.apps.${system}.bb_id_map.program}";
 in
 {
   programs.bash = {
     enable = true;
   };
-  systemd.user.services.bbh = {
+  systemd.user.services.bb_hitters = {
     Unit.Description = "bb_hitters";
     Install.WantedBy = [ "default.target" ];
     Service = {
       WorkingDirectory = "/home/andrew/code/fantasy";
       Environment = [
-        "STREAMLIT_SERVER_HEADLESS=true"
         "STREAMLIT_SERVER_PORT=8520"
-        "STREAMLIT_BROWSER_GATHER_USAGE_STATS=false"
       ];
-      ExecStart = "${streamlit} run ${fantasy}/bin/bb_hitters_2022.py";
+      ExecStart = "${bb_hitters}";
     };
   };
-  systemd.user.services.bbp = {
+  systemd.user.services.bb_pitchers = {
     Unit.Description = "bb_pitchers";
     Install.WantedBy = [ "default.target" ];
     Service = {
       WorkingDirectory = "/home/andrew/code/fantasy";
       Environment = [
-        "STREAMLIT_SERVER_HEADLESS=true"
         "STREAMLIT_SERVER_PORT=8521"
-        "STREAMLIT_BROWSER_GATHER_USAGE_STATS=false"
       ];
-      ExecStart = "${streamlit} run ${fantasy}/bin/bb_pitchers_2022.py";
+      ExecStart = "${bb_pitchers}";
     };
   };
-  systemd.user.services.bbid = {
+  systemd.user.services.bb_id_map = {
     Unit.Description = "bb_id_map";
     Install.WantedBy = [ "default.target" ];
     Service = {
       WorkingDirectory = "/home/andrew/code/fantasy";
       Environment = [
-        "STREAMLIT_SERVER_HEADLESS=true"
         "STREAMLIT_SERVER_PORT=8522"
-        "STREAMLIT_BROWSER_GATHER_USAGE_STATS=false"
       ];
-      ExecStart = "${streamlit} run ${fantasy}/bin/bb_id_map.py";
+      ExecStart = "${bb_id_map}";
     };
   };
 }
