c3aff90acb5ce1b0fafc852f5fa8572fd2222ecf
Andrew Burkett <burkett.andrew@gmail.com>
2023-08-27 14:15:00 -0700

    This goes with prvious commit

--- home.nix
+++ home.nix
@@ -10,12 +10,6 @@
   # To figure this out you can comment out the line and see what version it
   # expected.
   home.stateVersion = "23.05"; 
-  home.packages = [
-      pkgs.borgbackup
-      pkgs.bottom
-      pkgs.fd
-      pkgs.ripgrep
-  ];
   home.sessionVariables = {
       EDITOR = "vim";
   };
