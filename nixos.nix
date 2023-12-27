39eeae4fba7ff2e88cc0ac99314e4985e7764178
Andrew Burkett <burkett.andrew@gmail.com>
2023-12-27 08:34:14 -0800

    Switch to gnome

--- nixos.nix
+++ nixos.nix
@@ -39,8 +39,10 @@
   services.xserver.enable = true;
 
   # Enable the LXQT Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.lxqt.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.andrew.enableGnomeKeyring = true;
 
   # Configure keymap in X11
   services.xserver = {
