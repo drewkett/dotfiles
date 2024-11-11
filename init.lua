31a7d0a1e2f3683ce2143d68dab73f4ea176b8e5
Andrew Burkett <burkett.andrew@gmail.com>
2024-11-10 20:44:05 -0800

    Update formatters

--- init.lua
+++ init.lua
@@ -200,8 +200,9 @@ require("lazy").setup({
                 },
                 formatters_by_ft = {
                     lua = { "stylua" },
-                    -- python = { "isort", "black" },
+                    python = { "ruff_format" },
                     rust = { "rustfmt", lsp_format = "fallback" },
+                    markdown = { "mdformat" },
                     -- javascript = { "prettierd", "prettier", stop_after_first = true },
                 },
             },
