8858460c26ae75eb651adf88959b58e716f7acd5
Andrew Burkett <burkett.andrew@gmail.com>
2024-11-10 17:34:52 -0800

    Update install script

---
+++ install_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p ~/.config/nvim
+ln -sf "${SCRIPT_DIR}/init.lua" ~/.config/nvim/
+ln -sf "${SCRIPT_DIR}/tmux.conf" ~/.tmux.conf
+ln -sf "${SCRIPT_DIR}/bashrc" ~/.bashrc
+
+git config --global core.excludesFile '~/.gitignore'
+ln -sf "${SCRIPT_DIR}/gitignore" ~/.gitignore
