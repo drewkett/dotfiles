7fa1201c3dea59de51df6d20eed67604af211657
drewkett <burkett.andrew@gmail.com>
2024-11-11 13:54:46 -0800

    Rename bashrc to shellrc
    Make it something that is just sourced from bashrc

--- install_linux.sh
+++ install_linux.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 mkdir -p ~/.config/nvim
 ln -sf "${SCRIPT_DIR}/init.lua" ~/.config/nvim/
 ln -sf "${SCRIPT_DIR}/tmux.conf" ~/.tmux.conf
-ln -sf "${SCRIPT_DIR}/bashrc" ~/.bashrc
+# ln -sf "${SCRIPT_DIR}/bashrc" ~/.bashrc
 
 git config --global core.excludesFile '~/.gitignore'
 ln -sf "${SCRIPT_DIR}/gitignore" ~/.gitignore
