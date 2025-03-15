0de6962348fdc0b542d28d08f704f378693adba6
drewkett <burkett.andrew@gmail.com>
2025-03-15 11:31:29 -0700

    Update cargo installs

--- cargo_installs.sh
+++ cargo_installs.sh
@@ -2,8 +2,16 @@
 
 set -euxo pipefail
 
+rustup update stable
 export RUSTFLAGS="-Ctarget-cpu=native"
-cargo install --no-track --root tmp ripgrep fd-find pueue bat broot starship
-cargo install --no-track --root tmp --git https://github.com/bvaisvil/zenith.git
-cargo install --no-track --root tmp --git https://github.com/drewkett/hcp.git
-sudo cp tmp/bin/* /usr/local/bin/
+# https://github.com/sts10/rust-command-line-utilities
+PACKAGES="ripgrep fd-find pueue bat broot starship cargo-update bottom dua jj-cli"
+cargo install $PACKAGES
+# cargo install-update -a
+
+#cargo install --no-track --root tmp ripgrep fd-find pueue bat broot starship
+#cargo install --no-track --root tmp jj-cli --bin jj
+#cargo install --no-track --root tmp --git https://github.com/bvaisvil/zenith.git
+
+#cargo install --no-track --root tmp --git https://github.com/drewkett/hcp.git
+#sudo cp tmp/bin/* /usr/local/bin/
