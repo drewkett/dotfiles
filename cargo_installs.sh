3408e8c1185859f306a2dca1b550e35d0f7bdd15
Andrew Burkett <burkett.andrew@gmail.com>
2025-03-15 11:43:33 -0700

    Update cargo install. Rename install.sh

--- cargo_installs.sh
+++ cargo_installs.sh
@@ -3,13 +3,13 @@
 set -euxo pipefail
 
 rustup update stable
-export RUSTFLAGS="-Ctarget-cpu=native"
+
 # https://github.com/sts10/rust-command-line-utilities
 PACKAGES="ripgrep fd-find pueue bat broot starship cargo-update bottom dua jj-cli"
 cargo install $PACKAGES
+# To update
 # cargo install-update -a
 
-#cargo install --no-track --root tmp ripgrep fd-find pueue bat broot starship
 #cargo install --no-track --root tmp jj-cli --bin jj
 #cargo install --no-track --root tmp --git https://github.com/bvaisvil/zenith.git
 
--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ ln -sf "${SCRIPT_DIR}/init.lua" ~/.config/nvim/
 ln -sf "${SCRIPT_DIR}/tmux.conf" ~/.tmux.conf
 # ln -sf "${SCRIPT_DIR}/bashrc" ~/.bashrc
 
-git config --global core.excludesFile '~/.gitignore'
+ln -sf "${SCRIPT_DIR}/gitconfig" ~/.gitconfig
 ln -sf "${SCRIPT_DIR}/gitignore" ~/.gitignore
+ln -sf "${SCRIPT_DIR}/cargo_config.toml" ~/.cargo/config.toml
