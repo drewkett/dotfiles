47267461b526067640cf650058f6fa29b8c7317c
Andrew Burkett <burkett.andrew@gmail.com>
2025-03-15 12:03:05 -0700

    dua-cli, not dua

--- cargo_installs.sh
+++ cargo_installs.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 rustup update stable
 
 # https://github.com/sts10/rust-command-line-utilities
-PACKAGES="ripgrep fd-find pueue bat broot starship cargo-update bottom dua jj-cli"
+PACKAGES="ripgrep fd-find pueue bat broot starship cargo-update bottom dua-cli jj-cli"
 cargo install $PACKAGES
 # To update
 # cargo install-update -a
